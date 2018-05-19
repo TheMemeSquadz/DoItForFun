88fefb0f22671d194f720afaa794824acf928723
TheMemeSquadz <albernpacman@gmail.com>
2018-05-19 17:04:08 +0800

    first commit

--- sketch_may19d/sketch_may19d.ino
+++ sketch_may19d/sketch_may19d.ino
@@ -4,14 +4,14 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   lcd.begin(16, 2);
-  lcd.print("I Like Kampua &");
+  lcd.print("Fk Alai ");
   delay(5000);
 }
 
 void loop() {
   lcd.clear();
   lcd.setCursor(2,0);
-  lcd.print("I Like Chicken");
+  lcd.print("Fk Alai ");
   delay(3000);
   lcd.clear();
   delay(1000);
